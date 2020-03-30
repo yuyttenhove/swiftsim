7b80e713352137083b9e88e60099e9c07cb3377d
John Helly <j.c.helly@durham.ac.uk>
2020-03-30 16:27:08 +0100

    Modified MPI mesh code to use padding needed by FFTW
    Also changed some data types: x/y/z mesh coordinates are
    now ints and 1D indexes into the mesh are size_t.

--- src/mpi_mesh_gravity.h
+++ src/mpi_mesh_gravity.h
@@ -28,10 +28,12 @@
 #include "space.h"
 
 /**
- * @brief Returns 1D index of a 3D NxNxN array using row-major style.
+ * @brief Returns 1D index of a FFTW-padded 3D NxNxN array using row-major style.
  *
  * Wraps around in the corresponding dimension if any of the 3 indices is >= N
- * or < 0. Note that indexes are of type size_t in this version!
+ * or < 0. Note that indexes are of type size_t in this version and the array
+ * is assumed to be padded to size 2*(N/2+1) in the last dimension as required
+ * by FFTW MPI routines.
  *
  * @param i Index along x.
  * @param j Index along y.
@@ -39,10 +41,54 @@
  * @param N Size of the array along one axis.
  */
 __attribute__((always_inline, const)) INLINE static size_t
-row_major_id_periodic_size_t(const size_t i, const size_t j, const size_t k,
-                             const size_t N) {
+row_major_id_periodic_size_t_padded(const int i, const int j, const int k,
+                                    const int N) {
+  /* Find last two dimensions of the padded array */
+  const size_t Nj = N;
+  const size_t Nk = 2*(N/2+1);
+  
+  /* Get box-wrapped coordinates (note that we don't use the padded size here) */
+  const size_t i_wrap = (size_t) ((i + N) % N);
+  const size_t j_wrap = (size_t) ((j + N) % N);
+  const size_t k_wrap = (size_t) ((k + N) % N);
 
-  return (((i + N) % N) * N * N + ((j + N) % N) * N + ((k + N) % N));
+  /* Compute index in the padded array */
+  return (i_wrap*Nj*Nk) + (j_wrap*Nk) + k_wrap;
+}
+
+/**
+ * @brief Return i coordinate from an id returned by row_major_id_periodic_size_t_padded
+ *
+ * This extracts the index in the first dimension from a row major id
+ * returned by row_major_id_periodic_size_t_padded. I.e. it finds the
+ * 'i' input parameter that was used to generate the id.
+ *
+ * @param id The padded row major ID.
+ * @param N Size of the array along one axis.
+ */
+__attribute__((always_inline, const)) INLINE static int
+get_xcoord_from_padded_row_major_id(const size_t id, const int N) {
+  const size_t Nj = N;
+  const size_t Nk = 2*(N/2+1);
+  return (int) (id / (Nj*Nk));
+}
+
+/**
+ * @brief Convert a global mesh array index to local slice index
+ * 
+ * Given an index into the padded N*N*2*(N/2+1) array, compute
+ * the corresponding index in the slice of the array stored
+ * on the local MPI rank.
+ *
+ * @param id The padded row major ID.
+ * @param N Size of the array along one axis.
+ * @param slice_offset Index of the first slice on this rank
+ */
+__attribute__((always_inline, const)) INLINE static int
+get_index_in_local_slice(const size_t id, const int N, const int slice_offset) {
+  const size_t Nj = N;
+  const size_t Nk = 2*(N/2+1);
+  return id - ((size_t) slice_offset)*Nj*Nk;
 }
 
 /**
