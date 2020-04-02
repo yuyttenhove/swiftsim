37df0c1d63dc1129c9700bb2c7bd578fec9189c0
John Helly <j.c.helly@durham.ac.uk>
2020-04-02 13:06:35 +0100

    Modify mesh_apply_Green_function to work with the MPI mesh
    The function now takes two extra parameters whcih specify
    what slice of the mesh we're working on in the x direction.

--- src/mesh_gravity.c
+++ src/mesh_gravity.c
@@ -365,6 +365,8 @@ struct Green_function_data {
   double green_fac;
   double a_smooth2;
   double k_fac;
+  int slice_offset;
+  int slice_width;
 };
 
 /**
@@ -389,8 +391,11 @@ void mesh_apply_Green_function_mapper(void* map_data, const int num,
   const double a_smooth2 = data->a_smooth2;
   const double k_fac = data->k_fac;
 
-  /* Range handled by this call */
-  const int i_start = (fftw_complex*)map_data - frho;
+  /* Find what slice of the full mesh is stored on this MPI rank */
+  const int slice_offset = data->slice_offset;
+
+  /* Range of x coordinates in the full mesh handled by this call */
+  const int i_start = ((fftw_complex*)map_data - frho) + slice_offset;
   const int i_end = i_start + num;
 
   /* Loop over the x range corresponding to this thread */
@@ -438,7 +443,7 @@ void mesh_apply_Green_function_mapper(void* map_data, const int num,
         const double total_cor = green_cor * CIC_cor4;
 
         /* Apply to the mesh */
-        const int index = N * (N_half + 1) * i + (N_half + 1) * j + k;
+        const int index = N * (N_half + 1) * (i - slice_offset) + (N_half + 1) * j + k;
         frho[index][0] *= total_cor;
         frho[index][1] *= total_cor;
       }
@@ -455,11 +460,14 @@ void mesh_apply_Green_function_mapper(void* map_data, const int num,
  * @param tp The threadpool.
  * @param frho The NxNx(N/2) complex array of the Fourier transform of the
  * density field.
+ * @param slice_offset The x coordinate of the start of the slice on this MPI rank
+ * @param slice_width The width of the local slice on this MPI rank
  * @param N The dimension of the array.
  * @param r_s The Green function smoothing scale.
  * @param box_size The physical size of the simulation box.
  */
 void mesh_apply_Green_function(struct threadpool* tp, fftw_complex* frho,
+                               const int slice_offset, const int slice_width,
                                const int N, const double r_s,
                                const double box_size) {
 
@@ -470,21 +478,21 @@ void mesh_apply_Green_function(struct threadpool* tp, fftw_complex* frho,
   data.green_fac = -1. / (M_PI * box_size);
   data.a_smooth2 = 4. * M_PI * M_PI * r_s * r_s / (box_size * box_size);
   data.k_fac = M_PI / (double)N;
+  data.slice_offset = slice_offset;
+  data.slice_width = slice_width;
 
   /* Parallelize the Green function application using the threadpool
      to split the x-axis loop over the threads.
      The array is N x N x (N/2). We use the thread to each deal with
      a range [i_min, i_max[ x N x (N/2) */
-  if (N < 32) {
-    mesh_apply_Green_function_mapper(frho, N, &data);
-  } else {
-    threadpool_map(tp, mesh_apply_Green_function_mapper, frho, N,
-                   sizeof(fftw_complex), threadpool_auto_chunk_size, &data);
-  }
+  threadpool_map(tp, mesh_apply_Green_function_mapper, frho, slice_width,
+                 sizeof(fftw_complex), threadpool_auto_chunk_size, &data);
 
-  /* Correct singularity at (0,0,0) */
-  frho[0][0] = 0.;
-  frho[0][1] = 0.;
+  /* Correct singularity at (0,0,0), if it's in our local slice */
+  if(slice_offset==0) {
+    frho[0][0] = 0.;
+    frho[0][1] = 0.;
+  }
 }
 
 #endif
@@ -757,10 +765,6 @@ void pm_mesh_compute_potential(struct pm_mesh* mesh, const struct space* s,
   MPI_Allreduce(MPI_IN_PLACE, &max_frac_diff, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
   message("Maximum Fourier transform fractional difference = %e\n",
           max_frac_diff);
-
-  /* Discard MPI mesh for now*/
-  fftw_free(rho_slice);
-  fftw_free(frho_slice);
 #endif
 
   /* frho now contains the Fourier transform of the density field */
@@ -769,13 +773,65 @@ void pm_mesh_compute_potential(struct pm_mesh* mesh, const struct space* s,
   tic = getticks();
 
   /* Now de-convolve the CIC kernel and apply the Green function */
-  mesh_apply_Green_function(tp, frho, N, r_s, box_size);
+  mesh_apply_Green_function(tp, frho, 0, N, N, r_s, box_size);
 
   if (verbose)
     message("Applying Green function took %.3f %s.",
             clocks_from_ticks(getticks() - tic), clocks_getunit());
 
   tic = getticks();
+
+#ifdef WITH_MPI
+  /* Apply Green function to local slice of the MPI mesh */
+  tic = getticks();
+
+  mesh_apply_Green_function(tp, frho_slice, local_0_start, local_n0, N, r_s, box_size);
+  if (verbose)
+    message("Applying Green function to MPI mesh took %.3f %s.",
+            clocks_from_ticks(getticks() - tic), clocks_getunit());
+
+  tic = getticks();
+
+  /* Check we have the same values after the green function etc */
+  max_frac_diff = 0.0;
+  for (int i = local_0_start; i < local_0_start + local_n0; i += 1) {
+    for (int j = 0; j < N; j += 1) {
+      for (int k = 0; k < (N/2+1); k += 1) {
+        /* Compute corresponding coordinate in the non-MPI transform */
+        int i_full = j;
+        int j_full = i;
+        int k_full = k;
+        /* Find index into the full non-MPI transform array */
+        size_t index_full = i_full*N*(N/2+1) + j_full*(N/2+1) + k_full;
+        /* Find index into the local slice */
+        size_t Ns = N;
+        size_t index_slice = (i-local_0_start)*Ns*(Ns/2+1) + j*(Ns/2+1) + k;
+        /* Compare real parts */
+        double frac_diff;
+        double real_part_full  = frho[index_full][0];
+        double real_part_slice = frho_slice[index_slice][0];
+        frac_diff = fabs(real_part_full/real_part_slice-1.0);
+        if (frac_diff > max_frac_diff) {
+          max_frac_diff = frac_diff;
+        }
+        /* Compare imaginary parts */
+        double im_part_full  = frho[index_full][1];
+        double im_part_slice = frho_slice[index_slice][1];
+        frac_diff = fabs(im_part_full/im_part_slice-1.0);
+        if (frac_diff > max_frac_diff) {
+          max_frac_diff = frac_diff;
+        }
+      }
+    }
+  }
+  MPI_Allreduce(MPI_IN_PLACE, &max_frac_diff, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+  message("Maximum fractional difference in FT of potential = %e\n",
+          max_frac_diff);
+
+  /* Discard MPI mesh for now*/
+  fftw_free(rho_slice);
+  fftw_free(frho_slice);
+#endif
 
   /* Fourier transform to come back from magic-land */
   fftw_execute(inverse_plan);
