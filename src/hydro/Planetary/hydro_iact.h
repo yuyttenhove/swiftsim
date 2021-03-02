47b85d1fc770a7ea4458c511b43fc2b802bce1ba
Sergio Ruiz-Bonilla <dc-ruiz1@login7b.pri.cosma7.alces.network>
2021-03-02 20:50:23 +0000

    Modify imbalance factor

--- src/hydro/Planetary/hydro_iact.h
+++ src/hydro/Planetary/hydro_iact.h
@@ -120,13 +120,14 @@ __attribute__((always_inline)) INLINE static void runner_iact_density(
 
   /* Imbalance factor */
   //pi->imbalance.N_neig += 1.f;
-  pi->N_neig += 1.f;
   //pi->imbalance.rij_max = max(pi->imbalance.rij_max, r);
+  if (pi->mat_id == pj->mat_id){
+  pi->N_neig += 1.f;
   pi->rij_max = max(pi->rij_max, r);
   pi->sum_rij[0] += (pj->x[0] - pi->x[0]);
   pi->sum_rij[1] += (pj->x[1] - pi->x[1]);
   pi->sum_rij[2] += (pj->x[2] - pi->x[2]);
-
+  }
   /*if (pi->id == 854606){
     printf(
       "Print loop: "
@@ -147,12 +148,14 @@ __attribute__((always_inline)) INLINE static void runner_iact_density(
   
 
   //pj->imbalance.N_neig += 1.f;
-  pj->N_neig += 1.f;
   //pj->imbalance.rij_max = max(pj->imbalance.rij_max, r);
+  if (pi->mat_id == pj->mat_id){
+  pj->N_neig += 1.f;
   pj->rij_max = max(pj->rij_max, r);
   pj->sum_rij[0] += (pi->x[0] - pj->x[0]);
   pj->sum_rij[1] += (pi->x[1] - pj->x[1]);
   pj->sum_rij[2] += (pi->x[2] - pj->x[2]);
+  }
 }
 
 /**
@@ -218,13 +221,15 @@ __attribute__((always_inline)) INLINE static void runner_iact_nonsym_density(
   pi->density.rot_v[2] += faci * curlvr[2];
 
   /* Imbalance factor */
-  pi->N_neig += 1.f; // not error here?
   //pi->imbalance.N_neig += 1.f; // error here?
   //pi->imbalance.rij_max = max(pi->imbalance.rij_max, r);
+  if (pi->mat_id == pj->mat_id){
+  pi->N_neig += 1.f; // not error here?
   pi->rij_max = max(pi->rij_max, r);
   pi->sum_rij[0] += (pj->x[0] - pi->x[0]);
   pi->sum_rij[1] += (pj->x[1] - pi->x[1]);
   pi->sum_rij[2] += (pj->x[2] - pi->x[2]);
+  }
 }
 
 /**
