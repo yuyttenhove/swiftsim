1c4207e4e378ef7f4f83db98e48c1d71238daecf
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2021-03-11 17:00:42 +0100

    Add the new file defining the MPI tools to create packed particles

---
+++ src/mpi_part.h
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * This file is part of SWIFT.
+ * Copyright (c) 2016 Matthieu Schaller (matthieu.schaller@durham.ac.uk)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+#ifndef SWIFT_MPI_PART_H
+#define SWIFT_MPI_PART_H
+
+/* Config parameters. */
+#include "../config.h"
+
+/* Local headers */
+#include "error.h"
+#include "helper_macros.h"
+
+#define populate_offsets_(x) (((char *)&temp.x - (char *)&temp) / sizeof(char)),
+#define populate_lengths_(x) (sizeof(temp.x)),
+
+#define create_indexed_mpi_type(type, MPI_type, ...)                         \
+  ({                                                                         \
+    type temp;                                                               \
+                                                                             \
+    /* List of size of each field in the struct. */                          \
+    const int lengths[MACRO_NUM_ARGUMENTS(__VA_ARGS__)] = {                  \
+        MACRO_FOR_EACH(populate_lengths_, __VA_ARGS__)};                     \
+                                                                             \
+    /* List of offsets of each field from the start of the struct. */        \
+    const int offsets[MACRO_NUM_ARGUMENTS(__VA_ARGS__)] = {                  \
+        MACRO_FOR_EACH(populate_offsets_, __VA_ARGS__)};                     \
+                                                                             \
+    /* Create, register and resize the new type */                           \
+    if (MPI_Type_indexed(MACRO_NUM_ARGUMENTS(__VA_ARGS__), lengths, offsets, \
+                         MPI_BYTE, &MPI_type) != MPI_SUCCESS) {              \
+      error("Failed to create indexed MPI type for " #type);                 \
+    }                                                                        \
+    if (MPI_Type_commit(&MPI_type) != MPI_SUCCESS) {                         \
+      error("Failed to commit indexed MPI type for " #type);                 \
+    }                                                                        \
+    if (MPI_Type_create_resized(MPI_type, 0, sizeof(type), &MPI_type) !=     \
+        MPI_SUCCESS) {                                                       \
+      error("Failed to resize MPI type for " #type);                         \
+    }                                                                        \
+  })
+
+#endif /* SWIFT_MPI_PART_H */
