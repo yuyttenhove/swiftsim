9d91081fce60072df2cf2fe04523288b539d66e6
Yolan Uyttenhove <yolan.uyttenhove@ugent.be>
2021-05-11 17:32:55 +0200

    make hydro functions recursive (density)

--- src/shadowfax/cell_shadowfax.c
+++ src/shadowfax/cell_shadowfax.c
@@ -6,7 +6,26 @@ void cell_shadowfax_do_pair1_density_recursive(const struct engine *e,
                                                struct cell *restrict ci,
                                                struct cell *restrict cj,
                                                int sid, const double *shift) {
-  // TODO
+  int k;
+  /* recurse? */
+  if (ci->split) {
+    // TODO check if possible with max smoothing length
+    for (k = 0; k < 8; k++) {
+      if (ci->progeny[k] != NULL) {
+        cell_shadowfax_do_pair1_density_recursive(e, ci->progeny[k], cj, sid,
+                                                  shift);
+      }
+    }
+  } else if (cj->split) {
+    for (k = 0; k < 8; k++) {
+      if (cj->progeny[k] != NULL) {
+        cell_shadowfax_do_pair1_density_recursive(e, ci, cj->progeny[k], sid,
+                                                  shift);
+      }
+    }
+  } else {
+    cell_shadowfax_do_pair1_density(e, ci, cj, sid, shift);
+  }
 }
 
 void cell_shadowfax_do_pair_subset_density_recursive(
@@ -14,7 +33,49 @@ void cell_shadowfax_do_pair_subset_density_recursive(
     struct part *restrict parts_i, const int *restrict ind, int count,
     struct cell *restrict cj, const int sid, const int flipped,
     const double *shift) {
-  // TODO
+  int k;
+  /* recurse? */
+  if (ci->split) {
+    // TODO check if possible with max smoothing length
+    int sub_start_ind = 0;
+    for (k = 0; k < 8; k++) {
+      if (ci->progeny[k] != NULL) {
+        struct cell *sub = ci->progeny[k];
+        /* Does this sub contain some of the remaining particles? */
+        if (&parts_i[ind[sub_start_ind]] >= &sub->hydro.parts[0] &&
+            &parts_i[ind[sub_start_ind]] < &sub->hydro.parts[sub->hydro.count]) {
+          /* Does this sub contain all of the remaining particles? */
+          if (&parts_i[ind[count - 1]] < &sub->hydro.parts[sub->hydro.count]) {
+            cell_shadowfax_do_pair_subset_density_recursive(
+                e, sub, parts_i, ind, count, cj, sid, flipped, shift);
+          } else {
+            /* Some, but not all remaining particles are in this sub
+             * Find the number of particles in this sub. */
+            int sub_count = 1;
+            while (&parts_i[ind[sub_start_ind + sub_count]] <
+                   &sub->hydro.parts[sub->hydro.count]) {
+              sub_count++;
+            }
+            cell_shadowfax_do_pair_subset_density_recursive(
+                e, sub, parts_i, &ind[sub_start_ind], sub_count, cj, sid,
+                flipped, shift);
+            sub_start_ind += sub_count;
+          } /* Does this sub contain all of the remaining particles? */
+        }   /* Does this sub contain some of the remaining particles? */
+      }     /* Progeny not NULL? */
+    }
+  } else if (cj->split) {
+    for (k = 0; k < 8; k++) {
+      if (cj->progeny[k] != NULL) {
+        // TODO check if possible with max smoothing length
+        cell_shadowfax_do_pair_subset_density_recursive(
+            e, ci, parts_i, ind, count, cj->progeny[k], sid, flipped, shift);
+      }
+    }
+  } else {
+    cell_shadowfax_do_pair_subset_density(e, ci, parts_i, ind, count, cj, sid,
+                                          flipped, shift);
+  }
 }
 
 void cell_shadowfax_do_self1_density_recursive(const struct engine *e,
