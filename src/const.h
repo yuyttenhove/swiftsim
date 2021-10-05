e91f4781a8cbf9c4cdc3f81c2988ade029e3b362
Yolan Uyttenhove <yolan.uyttenhove@ugent.be>
2021-10-05 13:26:57 +0200

    test run

--- src/const.h
+++ src/const.h
@@ -83,24 +83,24 @@
 
 /* Types of gradients to use for SHADOWFAX_SPH */
 /* If no option is chosen, no gradients are used (first order scheme) */
-//#define SHADOWFAX_GRADIENTS
+#define SHADOWFAX_GRADIENTS
 
 /* SHADOWFAX_SPH slope limiters */
-//#define SHADOWFAX_SLOPE_LIMITER_PER_FACE
+#define SHADOWFAX_SLOPE_LIMITER_PER_FACE
 //#define SHADOWFAX_SLOPE_LIMITER_CELL_WIDE
-//#define SHADOWFAX_SLOPE_LIMITER_CELL_WIDE_EXACT
+#define SHADOWFAX_SLOPE_LIMITER_CELL_WIDE_EXACT
 
 /* Options to control SHADOWFAX_SPH */
 /* This option disables cell movement */
-#define SHADOWFAX_FIX_CELLS
+//#define SHADOWFAX_FIX_CELLS
 /* This option enables cell steering, i.e. trying to keep the cells regular by
    adding a correction to the cell velocities. See Springel 2010. */
-//#define SHADOWFAX_STEER_CELL_MOTION
+#define SHADOWFAX_STEER_CELL_MOTION
 /* This option enables the use of the hilbert ordering during the self density
  * tasks. Inserting particles into the delaunay tesselation according to their
  * hilbert ordering increases the efficiency of building the delaunay
  * tesselation */
-//#define SHADOWFAX_HILBERT_ORDERING
+#define SHADOWFAX_HILBERT_ORDERING
 /* This option evolves the total energy instead of the thermal energy */
 //#define SHADOWFAX_TOTAL_ENERGY
 /* This option performs a simple check whether or not a particle was already
